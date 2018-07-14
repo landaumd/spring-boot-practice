6f3d0beb4c2c7a3a466d3e584fc5194b5d0c5f1d
Megan Landau <landaumd@g.cofc.edu>
2018-07-14 13:44:55 -0700

    begin project

---
+++ src/main/java/com/meganlandau/repository/ShipwreckRepository.java
@@ -0,0 +1,8 @@
+package com.meganlandau.repository;
+
+import com.meganlandau.model.Shipwreck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+
+}
